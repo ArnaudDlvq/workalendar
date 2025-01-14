7ce9f7be9a3781d18ff8ba8da77b805918363dc8
Arnaud Delevacque <arnaud.delevacque@vroomly.com>
2025-01-14 13:42:24 +0100

    feat(romania): add epiphany and orthodox christmas for 2024 and beyond

--- workalendar/europe/romania.py
+++ workalendar/europe/romania.py
@@ -18,6 +18,7 @@ class Romania(OrthodoxCalendar):
     )
 
     # Christian holidays
+    # Epiphany not added before 2024
     include_good_friday = True
     include_easter_sunday = True
     include_easter_monday = True
@@ -28,8 +29,9 @@ class Romania(OrthodoxCalendar):
     include_christmas = True
     include_boxing_day = True
     boxing_day_label = 'Christmas Day'
-    # No Orthodox Christmas
+    # No Orthodox Christmas before 2024
     include_orthodox_christmas = False
+    orthodox_christmas_day_label = "Synaxis of St. John the Baptist"
 
     def get_childrens_day(self, year):
         """returns a possibly empty list of (date, holiday_name) tuples"""
@@ -49,8 +51,22 @@ class Romania(OrthodoxCalendar):
 
         return days
 
+    def get_epiphany_and_orthodox_christmas(self, year):
+        """returns a possibly empty list of (date, holiday_name) tuples"""
+        days = []
+        if year >= 2024:
+            epiphany_date = date(year, 1, 6)
+            orthodox_christmas_date = date(year, 1, 7)
+            days = [
+                (epiphany_date, "Epiphany"),
+                (orthodox_christmas_date, self.orthodox_christmas_day_label),
+            ]
+
+        return days
+
     def get_variable_days(self, year):
         days = super().get_variable_days(year)
         days.extend(self.get_childrens_day(year))
         days.extend(self.get_liberation_day(year))
+        days.extend(self.get_epiphany_and_orthodox_christmas(year))
         return days
